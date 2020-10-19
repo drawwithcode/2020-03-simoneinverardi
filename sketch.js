986e24d4421e264f78d4a427219256a6e104b0ea
simoneinverardi <71282425+simoneinverardi@users.noreply.github.com>
2020-10-19 18:26:49 +0200

    a

--- sketch.js
+++ sketch.js
@@ -1,12 +1,109 @@
-function preload(){
-  // put preload code here
+let angle = 0;
+let w = 25;
+let maxD;
+let song;
+let analyzer;
+
+function preload() {
+  song = loadSound('./assets/Water Drop Sound Effect.mp3');
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  let cnv = createCanvas(600, 600, WEBGL);
+  let xCnv = (windowWidth - width) / 2;
+  let yCnv = (windowHeight - height) / 2;
+  cnv.position(xCnv, yCnv);
+
+  maxD = dist(0, 0, 300, 300);
+
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(song);
 }
 
 function draw() {
-  // put drawing code here
+
+  background(255);
+  ortho(-500, 500, 500, -500, 0, 10000);
+  ambientLight(30, 30, 30);
+  pointLight(0, 255, 255, 10000, 0, 0);
+  pointLight(0, 255, 255, -10000, 0, 0);
+  pointLight(255, 0, 0, 0, 10000, 0);
+  pointLight(255, 0, 0, 0, -10000, 0);
+  rotateX(-PI / 4 * 3);
+  rotateY(-PI / 4 * 3);
+
+  let volume = 0;
+  if (mouseX > width/2) {
+    if (song.isPlaying() == false) {
+      song.play();
+    }
+    volume = analyzer.getLevel();
+    volume = map(volume,0,1,0,100);
+  } else {
+    song.stop();
+  }
+
+
+
+
+  let offset = 0;
+  for (let z = 0; z < height; z += w) {
+    for (let x = 0; x < width; x += w) {
+      push();
+      let d = dist(x, z, width / 2, height / 2);
+      let offset = map(d, 0, maxD, -PI, PI);
+      // let a = angle + offset;
+      let a = volume + offset;
+      let h = floor(map(sin(a), -1, 1, 100, 300));
+
+      noStroke();
+      specularMaterial(255,200);
+      shininess(20);
+      translate(x - width / 2, 0, z - height / 2);
+      box(w - 5, h, w);
+      pop();
+    }
+    offset += 0.2;
+  }
+  // angle += 0.1;
+  orbitControl(10, 10, 10);
 }
+//
+// function mouseClicked() {
+//   if (song.isPlaying()) {
+//     song.stop();
+//   } else {
+//     song.play();
+//     volume = analyzer.getLevel();
+//     volume = map(volume,0,1,0,height);
+//
+// }
+// }
+
+
+//
+// function addBox(){
+//   aNewBox = new Box(w - 2, h, w, 255, 200, 20);
+// }
+//
+// class Box {
+//   constructor(temp_w, temp_h, temp_d, temp_shader, temp_opacity, temp_shininess) {
+//     this.w = temp_w;
+//     this.h = temp_h;
+//     this.d = temp_d;
+//     this.color = temp_color;
+//     this.opacity = temp_opacity;
+//     this.shininess = temp_shininess;
+//   }
+//
+//   display() {
+//     push();
+//     noStroke();
+//     specularMaterial(255,200);
+//     shininess(20);
+//     translate(x - width / 2, 0, z - height / 2);
+//     box(w - 2, h, w);
+//     pop();
+//   }
+//
+// }
